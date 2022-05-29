9959b680f652e08929e9d399010ec5afdb1a754c
leng-z4 <kazuzero484@gmail.com>
2022-05-29 19:24:55 +0900

    [update]個人データ等の削除、微調整。

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, deleteU
 import { error } from './error.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLMdDi3V0UuReHYUqR_kRe9NpSBrzbf4g",
-    authDomain: "gace-id.firebaseapp.com",
-    projectId: "gace-id",
-    storageBucket: "gace-id.appspot.com",
-    messagingSenderId: "814051818099",
-    appId: "1:814051818099:web:951ad46d171c8c519ec806"
+    /* your config */
 };
 initializeApp(firebaseConfig);
 const auth = getAuth();
